87a2ebb35c67027167b196d68264a1977a45f5aa
Mitchel Santillan <mzs0122@tigermail.auburn.edu>
2020-02-03 17:35:20 -0600

    Update ExpressionEvaluator.java

--- ExpressionEvaluator.java
+++ ExpressionEvaluator.java
@@ -1,39 +1,42 @@
 import java.util.Scanner;
 import java.text.DecimalFormat;
-
-/**
-  *This prgram requires a combination of the Scanner as wel as the 
-  *DecimalFormat class.
-  *
-  *@author Mitchel Santillan
-  *@version 6/6/2019
-  */
 public class ExpressionEvaluator {
-   /**
-     *This program will calculate facts about numbers given by the user.
-     *
-     *@param args Command line arguments (not used).
-     */
    public static void main(String[] args) {
+      //Objects
       Scanner scan = new Scanner(System.in);
       DecimalFormat df = new DecimalFormat("#,##0.0####");
-   
+      
+      //variables    
+      String input = "";
+      double number = 0;  
+      double result = 0;    
+      //Receives user input
       System.out.print("Enter a value for x: ");
-      double num = scan.nextDouble();
-      double result = evaluator(num);
+      input = scan.nextLine(); 
+      number = Double.parseDouble(input);
+      result = getResult(number);
+
+      
+      //Outputs information based on data received.
       System.out.println("Result: " + result);
-      
-      String sResult = Double.toString(result);
-      int numLeft = sResult.indexOf('.');
-      int numRight = sResult.length() - (numLeft + 1);
-      
-      System.out.println("# digits to left of decimal point: " + numLeft);
-      System.out.println("# digits to right of decimal point: " + numRight);
+      System.out.println("# digits to left of decimal point: " + leftCheck(result));
+      System.out.println("#digits to right of decimal point: " + rightCheck(result));
       System.out.println("Formatted Result: " + df.format(result));
-   }   
+   }
    
-   public static double evaluator(double num) {
-      return (Math.sqrt(Math.abs(23.7 * Math.pow(num, 9)) - num)) 
-         / (7.3 * num * num + 5.2 * num + 3.1);
+   //The static modifier allows this method to be used across all methods in this class.
+   public static double getResult(double numberIn) {
+      return (Math.sqrt(Math.abs(23.7 * Math.pow(numberIn, 9)) - numberIn)) 
+         / (7.3 *Math.pow(numberIn, 2) + 5.2 * numberIn + 3.1);
+   }
+   
+   public static int leftCheck(double resultIn) {
+      String txt = Double.toString(resultIn);
+      return txt.indexOf('.');
+   }
+   
+   public static int rightCheck(double resultIn) {
+      String txt = Double.toString(resultIn);
+      return (txt.length()) - (txt.indexOf('.') + 1);
    }
 }
