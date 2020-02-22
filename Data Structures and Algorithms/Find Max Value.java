cd40ee1350cbd9bf86db4d26c206b98bbac09e36
Mitchel Santillan <mzs0122@tigermail.auburn.edu>
2020-02-22 15:40:47 -0600

    Last Modified: 2/22/2020

--- Data Structures and Algorithms/Find Max Value.java
+++ Data Structures and Algorithms/Find Max Value.java
@@ -4,22 +4,39 @@
   *@author Mitchel Santillan Cruz
   *@version 2/22/2020
   */
-public class MaxValue {
+public class Test {
    /**
-     *This program uses iteration to linearly traverse our array and find the maximum value
+     *This program uses iteration to linearly traverse our array and find the maximum value of an unsorted array
      *
      *@param args Command Line Arguments
      */
+   static int v_max = Integer.MIN_VALUE;
    public static void main(String[] args) {  
-      int[] arr = {5, 2, 5, 2, 9, 23, 64, 3, 7, 101};    
+      int[] arr = {5, 2, 5, 2, 9, 23, 64, 3, 7, 101};  
+        
+      //Test through iteration
       System.out.println(Max(arr, arr.length));
+      
+      //Test through recursion
+      System.out.println(Max2(arr, arr.length - 1));
    }
    
+   //Solving the given problem through ITERATION
    public static int Max(int[] arrIn, int elements) {           
-      int maxValue = Integer.MIN_VALUE;
-      for (int i = 0; i < arrIn.length; i++) {
-         if (arrIn[i] > maxValue)         maxValue = arrIn[i];
+      int maxValue = Integer.MIN_VALUE;                             //o(1)
+      for (int i = 0; i < arrIn.length; i++) {                      //o(n)
+         if (arrIn[i] > maxValue)         maxValue = arrIn[i];      //o(1) + o(1) = o(1)
       }
-      return maxValue;
+      return maxValue;                                              //o(1)
+   }
+   //When calculating run time for algorith, we can safely omit constants!
+   //Run Time: o(1) + o(n) + o(1) + o(1) = o(n) + o(1) = O(N) 
+   
+   //Solving the given problem through RECURSION
+   public static int Max2(int[] arrIn, int n) {
+      //Base Case
+      if (n < 0)                       return v_max;
+      else if (arrIn[n] > v_max)       v_max = arrIn[n];
+      return Max2(arrIn, n - 1);
    }
 }
