cdea2103bfade01421b1ca9e098057f4bbb50c85
Mitchel Santillan <mzs0122@tigermail.auburn.edu>
2020-03-02 20:51:53 -0600

    Last Modified: 3/2/2020

--- Programs/Pig Latin Converter.java
+++ Programs/Pig Latin Converter.java
@@ -1,32 +1,68 @@
 /**
-  *This program converts the users sentence to pig-latin!
+  *This program takes in a sentence and converts it to pig-latin.
+  *However, if it finds a numemric somewhere in the sentence then 
+  *it will tell you to try again without a numeric value.
   *
-  *@author Mitchel Santillan Cruz
-  *@version 3/1/2020
-  */
+  *@Author Mitchel Santillan cruz
+  *@Version 3/2/2020
+  */   
 import java.util.Scanner;
 public class PigLatin {
-  /**
-    *Acts as the driver which runs the program and accomplishes its task by using iterators to process letters one by one
-    *
-    *@param args Command Line Arguments (not used)
-    */
+   /**
+     *This method acts as the driver which takes care of receiving the user input and calling the methods.
+     *
+     *Time Complexity: o(n^2)
+     *
+     *@param args Command Line Arguments (not used).
+     */
    public static void main(String[] args) {
-      Scanner scan = new Scanner(System.in);
-      String temp = "";       
+      Scanner scan = new Scanner(System.in);                   
       System.out.print("Enter a sentence and I'll make it pig-latiny: ");
-      String sentence = scan.nextLine();    
-      Scanner analyze = new Scanner(sentence);       
-      while (analyze.hasNext()) {
-         temp += pigLatin(analyze.next()) + " ";
-      }      
-      System.out.println("Result: " + temp);
+      String sentence = scan.nextLine();            
+      System.out.println("Result: " + convertSentence(sentence));                                        
+   }
+     
+   /**
+     *This method checks if the input is a string by processing each item one by one (Iterator).
+     *
+     *Time Complexity: o(n^2)
+     *
+     *@param input Value of Type String
+     */  
+   public static void checkIfString(String input) {  
+      Scanner scan = new Scanner(input);
+      while (scan.hasNext()) {
+         try {
+            checkForNumbers(scan.next());            
+         } catch (NumberFormatException e) {
+            //Nothing happens here...           
+         }
+      }    
+   }   
+   
+   /**
+     *This method checks each individual word for any numeric values,
+     *if it finds them then the program halts.
+     *
+     *Time Complexity: o(n)
+     *
+     *@param input Value type String
+     */
+   public static void checkForNumbers(String input) {
+      for (char i : input.toCharArray()) {         
+         if (Character.isDigit(i)) {
+            System.out.println("Invalid input, try again with non-numeric values.");
+            System.exit(0);
+         }
+      }
    }
    
    /**
-     *This method takes in a word and converts it to pig-latin using a few of String and character methods
+     *This method takes care of converting each individual word to pig-latin.
      *
-     *@param input Type String which acts as the word to be converted
+     *Time Complexity: o(1)
+     *     
+     *@param input Value Type String
      */
    public static String pigLatin(String input) {
       String copy = input.toLowerCase();
@@ -34,5 +70,22 @@ public class PigLatin {
       copy = copy.substring(1, copy.length());
       String temp = letter + "ay";
       return copy + temp;      
+   }
+   
+   /**
+     *This method converts the sentence into pig-latin.
+     *
+     *Time Complexity: o(n^2)
+     *
+     *@param input Value Type String
+     */
+   public static String convertSentence(String input) {
+      String temp = "";
+      checkIfString(input);                                         
+      Scanner analyze = new Scanner(input);       
+      while (analyze.hasNext()) {                        
+         temp += pigLatin(analyze.next()) + " ";
+      }
+      return temp;
    }
 }
